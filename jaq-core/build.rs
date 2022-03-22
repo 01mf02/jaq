c3561bc3cfb5f23f00585bb5f0e13fed43ca1479
Michael Färber <01mf02@gmail.com>
2022-03-22 13:58:37 +0100

    Use new parser to build standard library!

---
+++ jaq-core/build.rs
@@ -0,0 +1,11 @@
+//! Cache parsed standard library.
+
+fn main() {
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let dest_path = std::path::Path::new(&out_dir).join("std.bin");
+    let buffer = std::fs::File::create(dest_path).unwrap();
+
+    let std = include_str!("src/std.jq");
+    let std = jaq_parse::parse(std, jaq_parse::parse::parse_defs()).unwrap();
+    bincode::serialize_into(buffer, &std).unwrap();
+}
--- jaq-core/src/lib.rs
+++ jaq-core/src/lib.rs
@@ -21,13 +21,14 @@ pub mod preprocess;
 pub mod recurse;
 pub mod toplevel;
 pub mod val;
+pub mod parse2;
 
 pub use error::Error;
 pub use filter::Filter;
 pub use path::Path;
 pub use preprocess::{ClosedFilter, OpenFilter, PreFilter};
 pub use recurse::Recurse;
-pub use toplevel::{Definition, Main, Module};
+pub use toplevel::{Definition, Definitions, Main, Module};
 pub use val::Val;
 
 use alloc::{boxed::Box, rc::Rc};
@@ -47,7 +48,18 @@ pub type ValRs<'a> = Box<dyn Iterator<Item = ValR> + 'a>;
 /// A stream of reference-counted value results.
 pub type RValRs<'a> = Box<dyn Iterator<Item = RValR> + 'a>;
 
-#[cfg(feature = "parse")]
+#[cfg(feature = "bincode")]
+fn parse_std() -> jaq_parse::parse::Defs {
+    let std = include_bytes!(concat!(env!("OUT_DIR"), "/std.bin"));
+    bincode::deserialize(std).unwrap()
+}
+
+#[cfg(not(feature = "bincode"))]
+fn parse_std() -> jaq_parse::parse::Defs {
+    let std = include_str!("std.jq");
+    jaq_parse::parse(std, jaq_parse::parse::parse_defs())
+}
+
 pub fn std() -> Module {
-    Module::parse(include_str!("std.jq")).unwrap()
+    Module::new(Definitions::try_from(parse_std()).unwrap())
 }
