4c0e536ce3f53122d353dc7a3e3e82322d5d00ed
Michael Färber <01mf02@gmail.com>
2022-04-07 09:04:47 +0200

    Document jaq-std.

--- jaq-std/src/lib.rs
+++ jaq-std/src/lib.rs
@@ -1,17 +1,31 @@
+//! Standard library for a JSON query language.
+//!
+//! Just like Rust, jaq is divided into a core and a standard library.
+//! The standard library provides a set of filters defined using core filters.
+//! For example, the standard library provides the `map(f)` filter,
+//! which is defined using the more elementary filter `[.[] | f]`.
+//!
+//! The time required to parse the standard library becomes evident
+//! when the runtime of the jaq filter is small.
+//! Therefore, when the "bincode" feature is enabled,
+//! this crate precompiles the standard library,
+//! in order to reduce startup time.
 #![no_std]
 
 extern crate alloc;
 use alloc::vec::Vec;
 
-#[cfg(feature = "bincode")]
+/// Return the standard library.
 pub fn std() -> Vec<jaq_parse::Def> {
-    // use preparsed standard library
-    let std = include_bytes!(concat!(env!("OUT_DIR"), "/std.bin"));
-    bincode::deserialize(std).unwrap()
-}
-
-#[cfg(not(feature = "bincode"))]
-pub fn std() -> Vec<jaq_parse::Def> {
-    let std = include_str!("std.jq");
-    jaq_parse::parse(std, jaq_parse::defs()).unwrap()
+    #[cfg(feature = "bincode")]
+    {
+        // use preparsed standard library
+        let std = include_bytes!(concat!(env!("OUT_DIR"), "/std.bin"));
+        bincode::deserialize(std).unwrap()
+    }
+    #[cfg(not(feature = "bincode"))]
+    {
+        let std = include_str!("std.jq");
+        jaq_parse::parse(std, jaq_parse::defs()).unwrap()
+    }
 }
