4e97b017ab7c641e529120084e08fa1295ae6095
Michael Färber <01mf02@gmail.com>
2022-04-15 16:55:42 +0200

    Implement more stuff for Int.

--- jaq-core/src/int.rs
+++ jaq-core/src/int.rs
@@ -1,7 +1,7 @@
-use core::fmt;
+use core::{cmp, fmt};
 
 /// Integer value.
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug)]
 pub struct Int {
     /// absolute value of the integer
     abs: usize,
@@ -10,12 +10,23 @@ pub struct Int {
 }
 
 impl Int {
-    pub fn is_positive(&self) -> bool {
+    /// Absolute value of the integer.
+    pub fn abs(self) -> usize {
+        self.abs
+    }
+
+    /// Return true if the integer is greater-equal zero.
+    pub fn is_positive(self) -> bool {
         self.pos
     }
 
-    pub fn abs(&self) -> usize {
-        self.abs
+    /// Convert integer to a floating-point value.
+    pub fn as_f64(self) -> f64 {
+        if self.pos {
+            self.abs as f64
+        } else {
+            -(self.abs as f64)
+        }
     }
 }
 
@@ -39,5 +50,36 @@ impl core::ops::Neg for Int {
     fn neg(mut self) -> Self {
         self.pos = !self.pos;
         self
+    }
+}
+
+impl PartialEq for Int {
+    fn eq(&self, other: &Self) -> bool {
+        if self.pos == other.pos {
+            self.abs == other.abs
+        } else {
+            self.abs == 0 && other.abs == 0
+        }
+    }
+}
+
+impl Eq for Int {}
+
+impl PartialOrd for Int {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Int {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        use cmp::Ordering::*;
+        match (self.pos, other.pos) {
+            (true, true) => self.abs.cmp(&other.abs),
+            (false, false) => self.abs.cmp(&other.abs).reverse(),
+            (true, false) | (false, true) if self.abs + other.abs == 0 => Equal,
+            (true, false) => Greater,
+            (false, true) => Less,
+        }
     }
 }
