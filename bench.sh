f3b1a2e37b5d56caca12ee1bd7b9e725a10ee045
Michael Färber <01mf02@gmail.com>
2022-10-04 19:08:52 +0200

    Update benchmark.

--- bench.sh
+++ bench.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-JQ=jq
-JAQ=target/release/jaq
+
+# Make sure that you are running a version of jq newer than 1.6,
+# otherwise this benchmark will likely take a looooooooong time.
+
+: "${JQ:=jq}"
+: "${JAQ:=target/release/jaq}"
+
 TIME='/usr/bin/time -f %U'
 
+# 2^16 =   65536
+# 2^17 =  131072
+# 2^20 = 1048576
 declare -a BENCHES=(
-'1000000 | [range(.)] | reverse'
-'1000000 | [range(.) | -.] | sort'
-'1000000 | [range(.) | [.]] | add'
-' 100000 | [range(.) | {(tostring): .}] | add'
-'   5000 | [range(.) | {(tostring): .}] | add | .[] += 1'
-' 100000 | [range(.) | {(tostring): .}] | add | with_entries(.value += 1)'
-'1000000 | [limit(.; repeat("a"))] | add | explode | implode'
-'1000000 | reduce range(.) as $x ([]; . + [$x + .[-1]])'
-'     16 | nth(.; 0 | recurse([., .])) | flatten'
-'     16 | nth(.; 0 | recurse([., .])) | (.. | scalars) |= .+1'
-' 100000 | [range(.) | tojson] | join(",") | "[" + . + "]" | fromjson'
+'1048576 | [range(.)] | reverse'
+'1048576 | [range(.) | -.] | sort'
+'1048576 | [range(.) | [.]] | add'
+' 131072 | [range(.) | {(tostring): .}] | add'
+' 131072 | [range(.) | {(tostring): .}] | add | .[] += 1'
+' 131072 | [range(.) | {(tostring): .}] | add | with_entries(.value += 1)'
+'1048576 | [limit(.; repeat("a"))] | add | explode | implode'
+'1048576 | reduce range(.) as $x ([]; . + [$x + .[-1]])'
+'     17 | nth(.; 0 | recurse([., .])) | flatten'
+'     17 | nth(.; 0 | recurse([., .])) | (.. | scalars) |= .+1'
+'  65536 | [range(.) | tojson] | join(",") | "[" + . + "]" | fromjson'
 )
 
 
-echo -n '|' '`empty` (100 iterations)'
-echo -n '|' $($TIME bash -c "for n in {1..100}; do $JAQ -n 'empty'; done" 2>&1)
-echo -n '|' $($TIME bash -c "for n in {1..100}; do $JQ  -n 'empty'; done" 2>&1)
+echo -n '|' '`empty` (512 iterations)'
+echo -n '|' $($TIME bash -c "for n in {1..512}; do $JAQ -n 'empty'; done" 2>&1)
+echo -n '|' $($TIME bash -c "for n in {1..512}; do $JQ  -n 'empty'; done" 2>&1)
 echo    '|'
 
 for val in "${BENCHES[@]}"; do
