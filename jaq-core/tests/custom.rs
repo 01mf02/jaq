a9e4f7fe63d18410edd366a53830f3162c9891ae
Félix Saparelli <felix@passcod.name>
2023-04-07 00:11:23 +1200

    Test with sink

--- jaq-core/tests/custom.rs
+++ jaq-core/tests/custom.rs
@@ -1,13 +1,20 @@
 //! Tests for custom filters.
 
-use std::{iter, rc::Rc};
+use std::{iter::once, rc::Rc};
 
-use jaq_core::{parse, Ctx, Definitions, Error, RcIter, Val, CustomFilter};
+use itertools::Itertools;
+use jaq_core::{parse, Ctx, CustomFilter, Definitions, Error, RcIter, Val};
 use serde_json::{json, Value};
 
-pub fn yields<const N: usize>(defs: &Definitions, x: Value, f: &str, ys: [Value; N], err: Option<Error>) {
+pub fn yields<const N: usize>(
+    defs: &Definitions,
+    x: Value,
+    f: &str,
+    ys: [Value; N],
+    err: Option<Error>,
+) {
     let f = parse::parse(&f, parse::main()).0.unwrap();
-    
+
     let mut errs = Vec::new();
     let f = defs.clone().finish(f, Vec::new(), &mut errs);
     assert_eq!(errs, Vec::new());
@@ -23,25 +30,42 @@ pub fn yields<const N: usize>(defs: &Definitions, x: Value, f: &str, ys: [Value;
 }
 
 #[test]
-fn arity0() {
+fn arity0_source_only() {
     let mut defs = Definitions::core();
-    defs.insert_custom("natzero", 0, CustomFilter::new(
-        |_cv| {
-            Box::new(iter::once(Ok(Val::Int(0))))
-        },
-    ));
-    defs.insert_custom("nattwenty", 0, CustomFilter::new(
-        |_cv| {
-            Box::new(iter::once(Ok(Val::Int(20))))
-        },
-    ));
-    defs.insert_custom("hello", 0, CustomFilter::new(
-        |_cv| {
-            Box::new(iter::once(Ok(Val::Str(Rc::new("world".into())))))
-        },
-    ));
+    defs.insert_custom(
+        "natzero",
+        0,
+        CustomFilter::new(|_cv| Box::new(once(Ok(Val::Int(0))))),
+    );
+    defs.insert_custom(
+        "nattwenty",
+        0,
+        CustomFilter::new(|_cv| Box::new(once(Ok(Val::Int(20))))),
+    );
+    defs.insert_custom(
+        "hello",
+        0,
+        CustomFilter::new(|_cv| Box::new(once(Ok(Val::Str(Rc::new("world".into())))))),
+    );
 
     yields(&defs, Value::Null, "natzero", [json!(0)], None);
     yields(&defs, Value::Null, "nattwenty", [json!(20)], None);
     yields(&defs, Value::Null, "hello", [json!("world")], None);
+}
+
+#[test]
+fn arity0_and_sink() {
+    let mut defs = Definitions::core();
+    defs.insert_custom(
+        "str_rev",
+        0,
+        CustomFilter::new(|(_, val)| {
+            Box::new(once(
+                val.to_str().map(|s| Val::str(s.chars().rev().join(""))),
+            ))
+        }),
+    );
+
+    yields(&defs, json!("hello"), "str_rev", [json!("olleh")], None);
+    yields(&defs, json!(""), "str_rev", [json!("")], None);
 }
