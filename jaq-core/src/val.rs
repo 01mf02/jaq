3573a1dfed3090d1da7a8e7e461d463c6035623c
Michael Färber <01mf02@gmail.com>
2023-06-28 11:47:44 +0200

    Shorten more.

--- jaq-core/src/val.rs
+++ jaq-core/src/val.rs
@@ -589,13 +589,10 @@ impl core::ops::Mul for Val {
             (Int(x), Int(y)) => Ok(Int(x * y)),
             (Float(f), Int(i)) | (Int(i), Float(f)) => Ok(Float(f * i as f64)),
             (Float(x), Float(y)) => Ok(Float(x * y)),
-            (Str(s), Int(i)) | (Int(i), Str(s)) => Ok({
-                (i > 0)
-                    .then(|| Self::str(s.repeat(i as usize)))
-                    // string multiplication with negatives or 0 results in null
-                    // <https://jqlang.github.io/jq/manual/#Builtinoperatorsandfunctions>
-                    .unwrap_or(Null)
-            }),
+            (Str(s), Int(i)) | (Int(i), Str(s)) if i > 0 => Ok(Self::str(s.repeat(i as usize))),
+            // string multiplication with negatives or 0 results in null
+            // <https://jqlang.github.io/jq/manual/#Builtinoperatorsandfunctions>
+            (Str(_), Int(_)) | (Int(_), Str(_)) => Ok(Null),
             (Num(n), r) => Self::from_dec_str(&n) * r,
             (l, Num(n)) => l * Self::from_dec_str(&n),
             (l, r) => Err(Error::MathOp(l, MathOp::Mul, r)),
