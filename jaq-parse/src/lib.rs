dc84463d25033a1d0d0a08a0281c9a6b40e9aa71
Michael Färber <01mf02@gmail.com>
2023-07-17 19:39:32 +0200

    Move parsing entry functions.

--- jaq-parse/src/lib.rs
+++ jaq-parse/src/lib.rs
@@ -16,53 +16,11 @@ mod token;
 
 pub use def::{Arg, Def, Main};
 pub use ops::{MathOp, OrdOp};
-pub use parse::{defs, main};
+pub use parse::{Error, parse, defs, main};
 use path::Path;
 use token::Token;
 
-use alloc::{string::String, string::ToString, vec::Vec};
-use chumsky::prelude::*;
-
 type Span = core::ops::Range<usize>;
+
 /// An object with position information.
 pub type Spanned<T> = (T, Span);
-
-/// Lex/parse error.
-pub type Error = Simple<String>;
-
-fn lex() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
-    let comment = just("#").then(take_until(just('\n'))).padded();
-
-    parse::token()
-        .padded_by(comment.repeated())
-        .map_with_span(|tok, span| (tok, span))
-        .padded()
-        .repeated()
-}
-
-/// Parse a string with a given parser.
-///
-/// May produce `Some` output even if there were errors.
-pub fn parse<T, P>(src: &str, parser: P) -> (Option<T>, Vec<Error>)
-where
-    P: Parser<Token, T, Error = Simple<Token>> + Clone,
-{
-    let (tokens, lex_errs) = lex()
-        .then_ignore(end())
-        .recover_with(skip_then_retry_until([]))
-        .parse_recovery(src);
-
-    let (parsed, parse_errs) = if let Some(tokens) = tokens {
-        let len = src.chars().count();
-        let stream = chumsky::Stream::from_iter(len..len + 1, tokens.into_iter());
-        parser.then_ignore(end()).parse_recovery(stream)
-    } else {
-        (None, Vec::new())
-    };
-
-    let lex_errs = lex_errs.into_iter().map(|e| e.map(|c| c.to_string()));
-    let parse_errs = parse_errs.into_iter().map(|e| e.map(|tok| tok.to_string()));
-    let errs: Vec<_> = lex_errs.chain(parse_errs).collect();
-
-    (parsed, errs)
-}
--- jaq-parse/src/parse/mod.rs
+++ jaq-parse/src/parse/mod.rs
@@ -1,8 +1,51 @@
 mod def;
 mod filter;
-pub mod path;
+mod path;
 mod prec_climb;
 mod token;
 
 pub use def::{defs, main};
-pub use token::token;
+
+use chumsky::prelude::*;
+use crate::{Spanned, Token};
+use alloc::{string::String, string::ToString, vec::Vec};
+
+/// Lex/parse error.
+pub type Error = Simple<String>;
+
+fn lex() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
+    let comment = just("#").then(take_until(just('\n'))).padded();
+
+    token::token()
+        .padded_by(comment.repeated())
+        .map_with_span(|tok, span| (tok, span))
+        .padded()
+        .repeated()
+}
+
+/// Parse a string with a given parser.
+///
+/// May produce `Some` output even if there were errors.
+pub fn parse<T, P>(src: &str, parser: P) -> (Option<T>, Vec<Error>)
+where
+    P: Parser<Token, T, Error = Simple<Token>> + Clone,
+{
+    let (tokens, lex_errs) = lex()
+        .then_ignore(end())
+        .recover_with(skip_then_retry_until([]))
+        .parse_recovery(src);
+
+    let (parsed, parse_errs) = if let Some(tokens) = tokens {
+        let len = src.chars().count();
+        let stream = chumsky::Stream::from_iter(len..len + 1, tokens.into_iter());
+        parser.then_ignore(end()).parse_recovery(stream)
+    } else {
+        (None, Vec::new())
+    };
+
+    let lex_errs = lex_errs.into_iter().map(|e| e.map(|c| c.to_string()));
+    let parse_errs = parse_errs.into_iter().map(|e| e.map(|tok| tok.to_string()));
+    let errs: Vec<_> = lex_errs.chain(parse_errs).collect();
+
+    (parsed, errs)
+}
