1ff1ab96fc60a7ba2cb7cb71d9dcc7b200ca133e
Kai Klingenberg <k.klingenberg@gmail.com>
2023-07-20 17:21:24 -0400

    Add missing math filters

--- jaq-native/src/math.rs
+++ jaq-native/src/math.rs
@@ -24,9 +24,9 @@ pub fn as_int(v: Val, fun: &str) -> Result<i32, Error> {
 
 /// Build a 0-ary filter from a 1-ary math function.
 macro_rules! math_0_ary {
-    ($f: ident, $domain: expr, $codomain: expr) => {
-        (stringify!($f), 0, |_, cv| {
-            box_once($domain(cv.1, stringify!($f)).map(libm::$f).map($codomain))
+    ($name: expr, $f: expr, $domain: expr, $codomain: expr) => {
+        ($name, 0, |_, cv| {
+            box_once($domain(cv.1, $name).map($f).map($codomain))
         })
     };
 }
@@ -35,14 +35,14 @@ pub(crate) use math_0_ary;
 
 /// Build a 2-ary filter that ignores '.' from a 2-ary math function.
 macro_rules! math_2_ary {
-    ($f: ident, $domain1: expr, $domain2: expr, $codomain: expr) => {
-        (stringify!($f), 2, |args, cv| {
+    ($name: expr, $f: expr, $domain1: expr, $domain2: expr, $codomain: expr) => {
+        ($name, 2, |args, cv| {
             let xs = args.get(0);
             let ys = args.get(1);
             Box::new(xs.cartesian(ys, cv).map(|(x, y)| {
-                let x = $domain1(x.clone()?, stringify!($f))?;
-                let y = $domain2(y.clone()?, stringify!($f))?;
-                Ok($codomain(libm::$f(x, y)))
+                let x = $domain1(x.clone()?, $name)?;
+                let y = $domain2(y.clone()?, $name)?;
+                Ok($codomain($f(x, y)))
             }))
         })
     };
@@ -52,8 +52,8 @@ pub(crate) use math_2_ary;
 
 /// Build a 3-ary filter that ignores '.' from a 3-ary math function.
 macro_rules! math_3_ary {
-    ($f: ident, $domain1: expr, $domain2: expr, $domain3: expr, $codomain: expr) => {
-        (stringify!($f), 3, |args, cv| {
+    ($name: expr, $f: expr, $domain1: expr, $domain2: expr, $domain3: expr, $codomain: expr) => {
+        ($name, 3, |args, cv| {
             use itertools::Itertools;
             let xs = args.get(0).run(cv.clone());
             let ys: Vec<_> = args.get(1).run(cv.clone()).collect();
@@ -63,10 +63,10 @@ macro_rules! math_3_ary {
                     .cartesian_product(ys)
                     .cartesian_product(zs)
                     .map(|((x, y), z)| {
-                        let x = $domain1(x.clone()?, stringify!($f))?;
-                        let y = $domain2(y.clone()?, stringify!($f))?;
-                        let z = $domain3(z.clone()?, stringify!($f))?;
-                        Ok($codomain(libm::$f(x, y, z)))
+                        let x = $domain1(x.clone()?, $name)?;
+                        let y = $domain2(y.clone()?, $name)?;
+                        let z = $domain3(z.clone()?, $name)?;
+                        Ok($codomain($f(x, y, z)))
                     }),
             )
         })
@@ -74,3 +74,35 @@ macro_rules! math_3_ary {
 }
 
 pub(crate) use math_3_ary;
+
+/// Source: https://github.com/itchyny/gojq/blob/main/func.go#L1374
+pub fn drem(l: f64, r: f64) -> f64 {
+    let x = libm::remainder(l, r);
+    if x == 0.0 {
+        libm::copysign(x, l)
+    } else {
+        x
+    }
+}
+
+/// Source: https://github.com/golang/go/blob/master/src/math/logb.go
+pub fn logb(x: f64) -> f64 {
+    if x == 0.0 {
+        f64::NEG_INFINITY
+    } else if x.is_infinite() {
+        f64::INFINITY
+    } else if x.is_nan() {
+        x
+    } else {
+        libm::ilogb(x) as f64
+    }
+}
+
+pub fn pow10(x: f64) -> f64 {
+    libm::pow(10.0, x)
+}
+
+/// Source: https://github.com/jqlang/jq/blob/master/src/builtin.c#L132
+pub fn significand(x: f64) -> f64 {
+    libm::scalbn(x, -libm::ilogb(x))
+}
