9a44df98ee697fa6ac2aa898fd7effa5ac05e7ad
Dusk Banks <me@bb010g.com>
2023-11-22 22:07:25 -0800

    style: normalize assignment operator sorting

--- jaq-interpret/tests/tests.rs
+++ jaq-interpret/tests/tests.rs
@@ -9,8 +9,8 @@ use serde_json::json;
 fn update_assign() {
     let ab = |v| json!({"a": v, "b": 2});
     gives(ab(1), ".a  = (.a, .b)", [ab(1), ab(2)]);
-    gives(ab(1), ".a += (.a, .b)", [ab(2), ab(3)]);
     gives(ab(1), ".a |= (.+1, .)", [ab(2)]);
+    gives(ab(1), ".a += (.a, .b)", [ab(2), ab(3)]);
 }
 
 // here, jaq diverges from jq, which returns [3,6,4,8]!
