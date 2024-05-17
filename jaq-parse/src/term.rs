bbf75f10d66571578748557c3a30bb5d6f0aa7f9
Michael Färber <01mf02@gmail.com>
2024-05-17 15:53:56 +0200

    Parse definitions, less verbose error reporting.

--- jaq-parse/src/term.rs
+++ jaq-parse/src/term.rs
@@ -1,32 +1,56 @@
 use crate::lex::{Punct, Token};
 use crate::Delim;
 use jaq_syn::filter::KeyVal;
-use jaq_syn::{path, string};
+use jaq_syn::{path, string, Arg, Call, Def};
 
+// TODO: problem: we cannot get a position from a None
+// example: (0 | )
+// include a scope?
 type Error<'a> = (Expect, Option<&'a Token<&'a str>>);
-enum Expect {}
+#[derive(Debug)]
+pub enum Expect {
+    Keyword(&'static str),
+    Punct(Punct),
+    Var,
+    ElseOrEnd,
+    Term,
+    Key,
+    Ident,
+    Arg,
+    Nothing,
+}
+
+type Result<'a, T> = core::result::Result<T, Error<'a>>;
 
 pub(crate) struct Parser<'a> {
     i: core::slice::Iter<'a, Token<&'a str>>,
-    e: Vec<Error<'a>>,
+    pub e: Vec<Error<'a>>,
+    /// names of fold-like filters, e.g. "reduce" and "foreach"
+    fold: &'a [&'a str],
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub enum Term<S> {
+    #[default]
+    Id,
+    Recurse,
+
     Num(S),
     Str(string::Str<Self>),
     Arr(Option<Box<Self>>),
     Obj(Vec<KeyVal<Self>>),
-    Id,
-    Recurse,
+
     Neg(Box<Self>),
     Pipe(Box<Self>, Option<S>, Box<Self>),
     BinOp(Box<Self>, Vec<(S, Self)>),
-    IfThenElse(Box<Self>, Box<Self>, Option<Box<Self>>),
-    TryCatch(Box<Self>, Option<Box<Self>>),
+
     Fold(S, Box<Self>, S, Vec<Self>),
-    Var(S),
+    TryCatch(Box<Self>, Option<Box<Self>>),
+    IfThenElse(Box<Self>, Box<Self>, Option<Box<Self>>),
+
     Call(S, Vec<Self>),
+    Var(S),
+
     Key(S),
     Path(Box<Self>, Vec<(path::Part<Self>, path::Opt)>),
 }
@@ -35,21 +59,38 @@ pub enum Term<S> {
 ///
 /// Note that for example `reduce` is not part of this list,
 /// because it *can* appear at the beginning of an expression.
-const KEYWORDS: &[&str] = &["include", "import", "def", "as", "and", "or"];
+const KEYWORDS: &[&str] = &[
+    "include", "import", "def", "as", "and", "or", "catch", "then", "else", "end",
+];
 
 impl<'a> Parser<'a> {
     pub fn new(i: &'a [Token<&'a str>]) -> Self {
         Self {
             i: i.iter(),
             e: Vec::new(),
+            fold: &["reduce", "foreach"],
         }
     }
 
-    fn with<T>(&mut self, tokens: &'a [Token<&'a str>], f: impl FnOnce(&mut Self) -> T) -> T {
+    fn with<T: Default, F>(&mut self, tokens: &'a [Token<&'a str>], f: F) -> T
+    where
+        F: FnOnce(&mut Self) -> Result<'a, T>,
+    {
         let i = core::mem::replace(&mut self.i, tokens.iter());
-        let tm = f(self);
+        let y = match f(self) {
+            Ok(y) => {
+                if let Some(next) = self.i.next() {
+                    self.e.push((Expect::Nothing, Some(next)));
+                }
+                y
+            }
+            Err(e) => {
+                self.e.push(e);
+                T::default()
+            }
+        };
         self.i = i;
-        tm
+        y
     }
 
     fn maybe<T>(&mut self, f: impl Fn(&mut Self) -> Option<T>) -> Option<T> {
@@ -62,26 +103,41 @@ impl<'a> Parser<'a> {
         y
     }
 
-    fn sep_by1<T>(&mut self, punct: Punct, f: impl Fn(&mut Self) -> T) -> Vec<T> {
-        let mut ys = Vec::from([f(self)]);
-        loop {
-            match self.i.next() {
-                Some(Token::Punct(p, _)) if *p == punct => ys.push(f(self)),
-                None => break,
-                _ => todo!(),
-            }
+    fn try_maybe<T, F>(&mut self, f: F) -> Result<'a, Option<T>>
+    where
+        F: Fn(&mut Self) -> Result<'a, Option<T>>,
+    {
+        let i = self.i.clone();
+        let y = f(self)?;
+        // rewind to previous state in case of non-match
+        if y.is_none() {
+            self.i = i;
         }
-        ys
+        Ok(y)
     }
 
-    fn args<T>(&mut self, f: impl Fn(&mut Self) -> T + Copy) -> Vec<T> {
+    fn sep_by1<T, F>(&mut self, punct: Punct, f: F) -> Result<'a, Vec<T>>
+    where
+        F: Fn(&mut Self) -> Result<'a, T>,
+    {
+        let mut ys = Vec::from([f(self)?]);
+        loop {
+            match self.i.next() {
+                Some(Token::Punct(p, _)) if *p == punct => ys.push(f(self)?),
+                None => return Ok(ys),
+                next => return Err((Expect::Punct(punct), next)),
+            }
+        }
+    }
+
+    fn args<T>(&mut self, f: impl Fn(&mut Self) -> Result<'a, T> + Copy) -> Vec<T> {
         self.maybe(|p| match p.i.next() {
             Some(Token::Delim(Delim::Paren, tokens)) => {
                 Some(p.with(tokens, |p| p.sep_by1(Punct::Semicolon, f)))
             }
             _ => None,
         })
-        .unwrap_or_else(|| Vec::new())
+        .unwrap_or_default()
     }
 
     fn op(&mut self, with_comma: bool) -> Option<&'a str> {
@@ -101,18 +157,32 @@ impl<'a> Parser<'a> {
         })
     }
 
-    fn var(&mut self) -> &'a str {
+    fn punct1(&mut self, punct: Punct) -> Result<'a, &'a str> {
         match self.i.next() {
-            Some(Token::Word(x)) if x.starts_with('$') => *x,
-            _ => todo!(),
+            Some(Token::Punct(p, s)) if *p == punct => Ok(*s),
+            next => Err((Expect::Punct(punct), next)),
         }
     }
 
-    pub fn term_with_comma(&mut self, with_comma: bool) -> Term<&'a str> {
-        let head = self.atom();
+    fn keyword(&mut self, kw: &'static str) -> Result<'a, ()> {
+        match self.i.next() {
+            Some(Token::Word(w)) if *w == kw => Ok(()),
+            next => Err((Expect::Keyword(kw), next)),
+        }
+    }
+
+    fn var(&mut self) -> Result<'a, &'a str> {
+        match self.i.next() {
+            Some(Token::Word(x)) if x.starts_with('$') => Ok(*x),
+            next => Err((Expect::Var, next)),
+        }
+    }
+
+    pub fn term_with_comma(&mut self, with_comma: bool) -> Result<'a, Term<&'a str>> {
+        let head = self.atom_path()?;
         let mut tail = Vec::new();
         while let Some(op) = self.op(with_comma) {
-            tail.push((op, self.atom()));
+            tail.push((op, self.atom_path()?));
         }
 
         let tm = if tail.is_empty() {
@@ -121,67 +191,56 @@ impl<'a> Parser<'a> {
             Term::BinOp(Box::new(head), tail)
         };
 
-        let pipe = self.maybe(|p| match p.i.next() {
-            Some(Token::Op("|")) => Some(None),
+        let pipe = self.try_maybe(|p| match p.i.next() {
+            Some(Token::Op("|")) => Ok(Some(None)),
             Some(Token::Word("as")) => {
-                let x = p.var();
+                let x = p.var()?;
                 match p.i.next() {
-                    Some(Token::Op("|")) => Some(Some(x)),
-                    _ => None,
+                    Some(Token::Op("|")) => Ok(Some(Some(x))),
+                    next => Err((todo!(), next)),
                 }
             }
-            _ => None,
-        });
-        match pipe {
+            _ => Ok(None),
+        })?;
+        Ok(match pipe {
             None => tm,
-            Some(x) => Term::Pipe(Box::new(tm), x, Box::new(self.term_with_comma(with_comma))),
-        }
+            Some(x) => Term::Pipe(Box::new(tm), x, Box::new(self.term_with_comma(with_comma)?)),
+        })
     }
 
-    pub fn term(&mut self) -> Term<&'a str> {
-        self.term_with_comma(true)
-    }
-
-    fn atom(&mut self) -> Term<&'a str> {
-        let tm = match self.i.next() {
-            Some(Token::Op("-")) => Term::Neg(Box::new(self.atom())),
+    fn atom(&mut self) -> Result<'a, Term<&'a str>> {
+        Ok(match self.i.next() {
+            Some(Token::Op("-")) => Term::Neg(Box::new(self.atom_path()?)),
             Some(Token::Word("if")) => {
-                let if_ = self.term();
-                if !matches!(self.i.next(), Some(&Token::Word("then"))) {
-                    todo!();
-                }
-                let then_ = self.term();
+                let if_ = self.term()?;
+                self.keyword("then")?;
+                let then_ = self.term()?;
                 let else_ = match self.i.next() {
                     Some(Token::Word("else")) => {
-                        let else_ = self.term();
-                        if !matches!(self.i.next(), Some(&Token::Word("end"))) {
-                            todo!();
-                        }
+                        let else_ = self.term()?;
+                        self.keyword("end")?;
                         Some(else_)
                     }
                     Some(Token::Word("end")) => None,
-                    _ => todo!(),
+                    next => return Err((Expect::ElseOrEnd, next)),
                 };
                 Term::IfThenElse(Box::new(if_), Box::new(then_), else_.map(Box::new))
             }
             Some(Token::Word("try")) => {
-                let try_ = self.atom();
-                let catch = self.maybe(|p| match p.i.next() {
-                    Some(Token::Word("catch")) => Some(p.atom()),
-                    _ => None,
-                });
+                let try_ = self.atom_path()?;
+                let catch = self.try_maybe(|p| match p.i.next() {
+                    Some(Token::Word("catch")) => Ok(Some(p.atom_path()?)),
+                    _ => Ok(None),
+                })?;
                 Term::TryCatch(Box::new(try_), catch.map(Box::new))
             }
-            Some(Token::Word(fold @ ("reduce" | "foreach"))) => {
-                let xs = self.term();
-                assert!(matches!(self.i.next(), Some(Token::Word("as"))));
-                let x = self.var();
+            Some(Token::Word(fold)) if self.fold.contains(fold) => {
+                let xs = self.atom_path()?;
+                self.keyword("as")?;
+                let x = self.var()?;
                 let args = self.args(|p| p.term());
-                if args.is_empty() {
-                    todo!()
-                }
                 Term::Fold(*fold, Box::new(xs), x, args)
-            },
+            }
             Some(Token::Word(id)) if id.starts_with('$') => Term::Var(*id),
             Some(Token::Word(id)) if !KEYWORDS.contains(id) => {
                 let head = Term::Call(*id, self.args(|p| p.term()));
@@ -209,14 +268,18 @@ impl<'a> Parser<'a> {
             }
             Some(Token::Delim(Delim::Brace, tokens)) if tokens.is_empty() => Term::Obj(Vec::new()),
             Some(Token::Delim(Delim::Brace, tokens)) => self.with(tokens, |p| {
-                Term::Obj(p.sep_by1(Punct::Comma, |p| p.obj_entry()))
+                p.sep_by1(Punct::Comma, |p| p.obj_entry()).map(Term::Obj)
             }),
             Some(Token::Str(parts)) => Term::Str(string::Str {
                 fmt: None,
                 parts: self.str_parts(parts),
             }),
-            _ => todo!(),
-        };
+            next => return Err((Expect::Term, next)),
+        })
+    }
+
+    fn atom_path(&mut self) -> Result<'a, Term<&'a str>> {
+        let tm = self.atom()?;
 
         let tm = match self.opt() {
             path::Opt::Optional => Term::TryCatch(Box::new(tm), None),
@@ -226,36 +289,45 @@ impl<'a> Parser<'a> {
         let mut path: Vec<_> = core::iter::from_fn(|| self.path_part_opt()).collect();
         while self.punct(Punct::Dot).is_some() {
             use path::Opt;
-            let key = self.i.next().and_then(ident_key).unwrap();
+            let key = match self.i.next() {
+                Some(Token::Word(id)) if !id.starts_with(['$', '@']) => Some(*id),
+                next => return Err((Expect::Key, next)),
+            };
             let opt = self.punct(Punct::Question).is_some();
-            let key = Term::Str(string::Str::from(key.to_string()));
+            let key = Term::Str(string::Str::from(key.unwrap_or("").to_string()));
             let opt = if opt { Opt::Optional } else { Opt::Essential };
             path.push((path::Part::Index(key), opt));
             path.extend(core::iter::from_fn(|| self.path_part_opt()));
         }
-        if path.is_empty() {
+        Ok(if path.is_empty() {
             tm
         } else {
             Term::Path(Box::new(tm), path)
-        }
+        })
     }
 
-    fn obj_entry(&mut self) -> KeyVal<Term<&'a str>> {
-        self.maybe(|p| match p.i.next() {
+    fn term(&mut self) -> Result<'a, Term<&'a str>> {
+        self.term_with_comma(true)
+    }
+
+    fn obj_entry(&mut self) -> Result<'a, KeyVal<Term<&'a str>>> {
+        match self.i.next() {
             Some(Token::Word(k)) if !k.starts_with(['$', '@']) => {
                 let k = string::Str::from(k.to_string());
-                let v = p.punct(Punct::Colon).map(|_| p.term_with_comma(false));
-                Some(KeyVal::Str(k, v))
+                let v = self
+                    .punct(Punct::Colon)
+                    .map(|_| self.term_with_comma(false))
+                    .transpose()?;
+                Ok(KeyVal::Str(k, v))
             }
-            _ => None,
-        })
-        .unwrap_or_else(|| {
-            let k = self.atom();
-            if self.punct(Punct::Colon).is_none() {
-                todo!()
+            // TODO: handle $x
+            Some(Token::Delim(Delim::Paren, tokens)) => {
+                let k = self.with(tokens, |p| p.term());
+                self.punct1(Punct::Colon)?;
+                Ok(KeyVal::Filter(k, self.term()?))
             }
-            KeyVal::Filter(k, self.term())
-        })
+            next => Err((todo!(), next)),
+        }
     }
 
     fn str_parts(
@@ -272,24 +344,24 @@ impl<'a> Parser<'a> {
         parts.collect()
     }
 
-    fn path_part(&mut self) -> path::Part<Term<&'a str>> {
+    fn path_part(&mut self) -> Result<'a, path::Part<Term<&'a str>>> {
         use path::Part::{Index, Range};
-        if self.i.as_slice().is_empty() {
+        Ok(if self.i.as_slice().is_empty() {
             Range(None, None)
         } else if self.punct(Punct::Colon).is_some() {
-            Range(None, Some(self.term()))
+            Range(None, Some(self.term()?))
         } else {
-            let tm = self.term();
+            let tm = self.term()?;
             if self.punct(Punct::Colon).is_some() {
                 if self.i.as_slice().is_empty() {
                     Range(Some(tm), None)
                 } else {
-                    Range(Some(tm), Some(self.term()))
+                    Range(Some(tm), Some(self.term()?))
                 }
             } else {
                 Index(tm)
             }
-        }
+        })
     }
 
     fn path_part_opt(&mut self) -> Option<(path::Part<Term<&'a str>>, path::Opt)> {
@@ -306,6 +378,80 @@ impl<'a> Parser<'a> {
             opt = path::Opt::Optional;
         }
         opt
+    }
+
+    pub fn main(&mut self) -> Result<'a, Main<Term<&'a str>>> {
+        Ok(Main {
+            defs: self.defs()?,
+            body: self.i.as_slice(),
+        }
+        .map(&mut |tokens| self.with(tokens, |p| p.term())))
+    }
+
+    pub fn defs(&mut self) -> Result<'a, Vec<Def<Main<&'a [Token<&'a str>]>>>> {
+        core::iter::from_fn(|| self.def_head().map(|()| self.def_tail())).collect()
+    }
+
+    pub fn def_rhs(&mut self) -> Result<'a, Main<&'a [Token<&'a str>]>> {
+        let defs = self.defs()?;
+        let i = self.i.as_slice();
+        let is_semicolon = |tk| matches!(tk, &Token::Punct(Punct::Semicolon, _));
+        let body = match self.i.position(is_semicolon) {
+            None => return Err((Expect::Punct(Punct::Semicolon), None)),
+            Some(p) => &i[..p],
+        };
+        Ok(Main { defs, body })
+    }
+
+    fn def_head(&mut self) -> Option<()> {
+        self.maybe(|p| match p.i.next() {
+            Some(Token::Word("def")) => Some(()),
+            _ => None,
+        })
+    }
+
+    fn def_lhs(&mut self) -> Result<'a, Call> {
+        let name = match self.i.next() {
+            Some(Token::Word(name)) if !name.starts_with(['$', '@']) => name.to_string(),
+            next => return Err((Expect::Ident, next)),
+        };
+        let args = self.args(|p| {
+            Ok(match p.i.next() {
+                Some(Token::Word(v)) if v.starts_with('$') => Arg::Var(v.to_string()),
+                Some(Token::Word(arg)) if !arg.starts_with('@') => Arg::Fun(arg.to_string()),
+                next => return Err((Expect::Arg, next)),
+            })
+        });
+        Ok(Call { name, args })
+    }
+
+    fn def_tail(&mut self) -> Result<'a, Def<Main<&'a [Token<&'a str>]>>> {
+        let lhs = self.def_lhs()?;
+        self.punct1(Punct::Colon)?;
+        let rhs = self.def_rhs()?;
+
+        Ok(Def { lhs, rhs })
+    }
+}
+
+#[derive(Debug)]
+pub struct Main<F> {
+    /// Definitions at the top of the filter
+    pub defs: Vec<Def<Self>>,
+    /// Body of the filter, e.g. `[.[] | f]`.
+    pub body: F,
+}
+
+impl<F> Main<F> {
+    fn map<G>(self, f: &mut impl FnMut(F) -> G) -> Main<G> {
+        let defs = self.defs.into_iter().map(|def| Def {
+            lhs: def.lhs,
+            rhs: def.rhs.map(f),
+        });
+        Main {
+            defs: defs.collect(),
+            body: f(self.body),
+        }
     }
 }
 
