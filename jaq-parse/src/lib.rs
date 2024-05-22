da082566b6e3660976c5b7ece88e4371ec9f5935
Michael Färber <01mf02@gmail.com>
2024-05-22 11:52:17 +0200

    Restore old lexer.

--- jaq-parse/src/lib.rs
+++ jaq-parse/src/lib.rs
@@ -7,11 +7,11 @@ extern crate alloc;
 
 mod def;
 mod filter;
-mod lex;
+pub mod lex;
 mod path;
 mod prec_climb;
 mod string;
-mod term;
+pub mod term;
 mod token;
 
 pub use def::{defs, main};
@@ -19,9 +19,18 @@ use token::{Delim, Token};
 
 use alloc::{string::String, string::ToString, vec::Vec};
 use chumsky::prelude::*;
+use jaq_syn::Spanned;
 
 /// Lex/parse error.
 pub type Error = Simple<String>;
+
+fn lex() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
+    recursive(token::tree)
+        .map_with_span(|tree, span| tree.tokens(span))
+        .repeated()
+        .flatten()
+        .collect()
+}
 
 /// Parse a string with a given parser.
 ///
@@ -30,16 +39,12 @@ pub fn parse<T, P>(src: &str, parser: P) -> (Option<T>, Vec<Error>)
 where
     P: Parser<Token, T, Error = Simple<Token>> + Clone,
 {
-    let (tokens, lex_errs) = crate::lex::Lex::new(src).lex();
+    let (tokens, lex_errs) = lex()
+        .then_ignore(end())
+        .recover_with(skip_then_retry_until([]))
+        .parse_recovery(src);
 
-    let mut new_parser = term::Parser::new(&tokens);
-    std::println!("{:?}", new_parser.term());
-    std::println!("{:?}", new_parser.e);
-
-    let tokens: Vec<_> = tokens.into_iter().flat_map(|t| t.tokens(src)).collect();
-    //std::println!("Tokens: {tokens:?}");
-
-    let (parsed, parse_errs) = if lex_errs.is_empty() {
+    let (parsed, parse_errs) = if let Some(tokens) = tokens {
         let len = src.chars().count();
         let stream = chumsky::Stream::from_iter(len..len + 1, tokens.into_iter());
         parser.then_ignore(end()).parse_recovery(stream)
@@ -47,10 +52,7 @@ where
         (None, Vec::new())
     };
 
-    let lex_errs = lex_errs.iter().map(|(e, s)| {
-        let (e, span) = e.to_simple_error(s, src);
-        Simple::custom(span, e)
-    });
+    let lex_errs = lex_errs.into_iter().map(|e| e.map(|c| c.to_string()));
     let parse_errs = parse_errs.into_iter().map(|e| e.map(|tok| tok.to_string()));
     let errs: Vec<_> = lex_errs.chain(parse_errs).collect();
 
