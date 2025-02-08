3318e376e5c896e044fdf67a4c438877b1444318
Michael Färber <01mf02@gmail.com>
2025-02-08 18:47:29 +0100

    Initial work on man page.

---
+++ docs/filter.lua
@@ -0,0 +1,52 @@
+function Header(el)
+  -- uppercase top-level headings for man pages
+  if FORMAT == "man" and el.level == 1 then
+    return el:walk{Str = function(el) return pandoc.Str(string.upper(el.text)) end}
+  end
+end
+
+-- inline code is assumed to be in jq
+function Code(code)
+  code.classes[1] = "jq"
+  return code
+end
+
+-- code blocks are assumed to be in jq if no other language is given
+function CodeBlock(block)
+  if next(block.classes) == nil then
+    block.classes[1] = "jq"
+    return block
+  end
+end
+
+function Div(el)
+  local class = el.classes[1]
+  if class == "Examples" then
+    el = el:walk{CodeBlock = function(block) return example(block.text) end}
+
+    if FORMAT == "html" then
+      local summary = pandoc.Plain{
+        pandoc.RawInline("html",  '<summary>'), pandoc.Str 'Examples',
+        pandoc.RawInline("html", "</summary>")
+      }
+      return {
+        pandoc.RawBlock("html",  '<details>'), summary, el,
+        pandoc.RawBlock("html", '</details>')
+      }
+    end
+  end
+  if FORMAT == "man" then
+    el = pandoc.DefinitionList({{pandoc.Emph(class), el.content}})
+  end
+  return el
+end
+
+function example(test)
+  local _, _, filter, input, output = test:find("([^\n]+)\n([^\n]+)\n(.*)")
+
+  if FORMAT == "man" then
+    filter = "'" .. filter .. "'"
+    input = "'" .. input .. "'"
+    return pandoc.CodeBlock("$ jaq " .. filter .. " \\\n  <<< " .. input .. "\n" .. output)
+  end
+end
