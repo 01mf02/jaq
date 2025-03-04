ebc5b6d2961d51cf1c466e9b7794f8e611c4650f
Michael Färber <01mf02@gmail.com>
2025-03-04 12:33:31 +0100

    Actually compile fuzz target.

--- jaq-core/fuzz/fuzz_targets/parse_tokens.rs
+++ jaq-core/fuzz/fuzz_targets/parse_tokens.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 libfuzzer_sys::fuzz_target!(|tokens: Vec<jaq_core::load::lex::Token<&str>>| {
-    let parser = jaq_core::load::Parser::new(tokens.as_slice());
-    let _ = parser.parse(|p| p.defs());
-    let _ = parser.parse(|p| p.term());
+    let parser = || jaq_core::load::Parser::new(tokens.as_slice());
+    let _ = parser().parse(|p| p.defs());
+    let _ = parser().parse(|p| p.term());
 });
