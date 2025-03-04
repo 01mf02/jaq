624753dbff81e216c81f6a74b3468ce3b44fba4a
Michael Färber <01mf02@gmail.com>
2025-03-04 10:15:48 +0100

    Simplify load_and_compile.

--- jaq-core/fuzz/fuzz_targets/load_and_compile.rs
+++ jaq-core/fuzz/fuzz_targets/load_and_compile.rs
@@ -1,31 +1,22 @@
 #![no_main]
 
-use jaq_core::{
-    load::{Arena, File, Loader},
-    Compiler,
-};
+use jaq_core::load::{Arena, File, Loader};
+use jaq_core::Compiler;
 
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &str| {
-    if data.contains("\"") || data.contains("main") {
+fuzz_target!(|code: &str| {
+    if code.contains("\"") || code.contains("main") {
         return;
     }
 
-    let program = File {
-        code: data,
-        path: (),
-    };
-
+    let program = File { code, path: () };
     let loader = Loader::new(jaq_std::defs());
     let arena = Arena::default();
 
-    match loader.load(&arena, program) {
-        Ok(modules) => {
-            let _ = Compiler::default()
-                .with_funs(jaq_std::funs::<jaq_json::Val>())
-                .compile(modules);
-        }
-        _ => {}
+    if let Ok(modules) = loader.load(&arena, program) {
+        let _ = Compiler::default()
+            .with_funs(jaq_std::funs::<jaq_json::Val>())
+            .compile(modules);
     }
 });
