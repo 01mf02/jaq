c8a38219af2cb9fab611dcb18ea48f5026021e80
Michael Färber <01mf02@gmail.com>
2025-03-05 10:01:57 +0100

    Debugging code.

--- jaq-core/fuzz/fuzz_targets/parse_tokens.rs
+++ jaq-core/fuzz/fuzz_targets/parse_tokens.rs
@@ -1,6 +1,7 @@
 #![no_main]
 
 libfuzzer_sys::fuzz_target!(|tokens: Vec<jaq_core::load::lex::Token<&str>>| {
+    //println!("{tokens:?}");
     let parser = || jaq_core::load::Parser::new(tokens.as_slice());
     let _ = parser().parse(|p| p.defs());
     let _ = parser().parse(|p| p.term());
