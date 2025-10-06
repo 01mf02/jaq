80744d683041e4a962ef6edcd9e065a9bd92aa24
Michael Färber <01mf02@gmail.com>
2025-10-06 14:10:02 +0200

    Document eXtended JavaScript Object Notation (XJON).

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # use `man -l jaq.1` to read generated man page
-FILES="intro.md cli.md corelang.md stdlib.md"
+FILES="intro.md cli.md corelang.md stdlib.md formats.md"
 pandoc $FILES --to filter.lua | cargo run -- --run-tests
 pandoc $FILES --lua-filter filter.lua -s -o MANUAL.html
 pandoc man-prologue.md $FILES man-epilogue.md --lua-filter filter.lua -s -o jaq.1
