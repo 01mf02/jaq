4183594cb76e1d1cb8ed4c6b607f388474c09400
Michael Färber <01mf02@gmail.com>
2025-10-06 15:05:09 +0200

    Byte strings.

--- docs/build.sh
+++ docs/build.sh
@@ -2,5 +2,5 @@
 # use `man -l jaq.1` to read generated man page
 FILES="intro.md cli.md corelang.md stdlib.md formats.md"
 pandoc $FILES --to filter.lua | cargo run -- --run-tests
-pandoc $FILES --lua-filter filter.lua -s -o MANUAL.html
+pandoc htm-prologue.md $FILES --lua-filter filter.lua -s -o MANUAL.html
 pandoc man-prologue.md $FILES man-epilogue.md --lua-filter filter.lua -s -o jaq.1
