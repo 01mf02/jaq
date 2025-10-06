9f67708c80614a4573b5b91aa294af0cd45a9f12
Michael Färber <01mf02@gmail.com>
2025-10-06 19:27:40 +0200

    Make a nice TOC for the HTML version.

--- docs/build.sh
+++ docs/build.sh
@@ -2,5 +2,5 @@
 # use `man -l jaq.1` to read generated man page
 FILES="intro.md cli.md corelang.md stdlib.md formats.md"
 pandoc $FILES --to filter.lua | cargo run -- --run-tests
-pandoc htm-prologue.md $FILES --lua-filter filter.lua -s -o MANUAL.html
+pandoc htm-prologue.md $FILES --lua-filter filter.lua --template template.html --css style.css --toc --toc-depth 2 -s -o MANUAL.html
 pandoc man-prologue.md $FILES man-epilogue.md --lua-filter filter.lua -s -o jaq.1
