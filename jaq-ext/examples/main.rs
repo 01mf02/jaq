069ad64f1590a29ca66b41f5244ac565a25658ef
Michael Färber <01mf02@gmail.com>
2025-12-17 14:23:06 +0100

    Simplify jaq-ext example.

--- jaq-ext/examples/main.rs
+++ jaq-ext/examples/main.rs
@@ -2,9 +2,6 @@ use jaq_ext::{data, load, read, write};
 use std::io::{self, Error, ErrorKind};
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin().lock();
-    let stdout = &mut io::stdout().lock();
-
     let filter = std::env::args()
         .nth(1)
         .ok_or_else(|| Error::new(ErrorKind::InvalidInput, "expected filter as first argument"))?;
@@ -13,7 +10,8 @@ fn main() -> io::Result<()> {
             .for_each(|fr| eprint!("{}", load::FileReportsDisp::new(fr)));
         Error::from(ErrorKind::InvalidInput)
     })?;
-    let inputs = read::json::read_many(stdin);
+    let inputs = read::json::read_many(io::stdin().lock());
+    let stdout = &mut io::stdout().lock();
 
     let runner = Default::default();
     let vars = Default::default();
