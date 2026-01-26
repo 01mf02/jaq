b58b150d5bbd68974bbac9f7236a55e4be678c59
Michael Färber <01mf02@gmail.com>
2026-01-26 17:19:16 +0100

    Move tests.

--- jaq-std/tests/defs.rs
+++ jaq-std/tests/defs.rs
@@ -2,27 +2,13 @@
 
 pub mod common;
 
-use common::{give, gives};
+use common::give;
 use serde_json::json;
 
 #[test]
 fn add() {
     give(json!({"a": 1, "b": 2}), "add", json!(3));
     give(json!([[0, 1], [2, 3]]), "add", json!([0, 1, 2, 3]));
-}
-
-#[test]
-fn all() {
-    give(json!({"a": false, "b": true}), "all", json!(false));
-    give(json!({"a": 1, "b": 2}), "all", json!(true));
-
-    let f = "def positive(f): all(f; . > 0); positive(.[])";
-    give(json!([1, 2]), f, json!(true));
-}
-
-#[test]
-fn any() {
-    give(json!({"a": false, "b": true}), "any", json!(true));
 }
 
 // aliases for fromdateiso8601 and todateiso8601
@@ -75,20 +61,6 @@ yields!(
     "any(0, nan, infinite, -infinite, []; isnormal)",
     false
 );
-
-yields!(join_empty, r#"[] | join(" ")"#, "");
-yields!(
-    join_strs,
-    r#"["Hello", "world"] | join(" ")"#,
-    "Hello world"
-);
-yields!(join_nums, r#"[2, 3, 4, 5] | join(",")"#, "2,3,4,5");
-
-yields!(map, "[1, 2] | map(.+1)", [2, 3]);
-
-// this diverges from jq, which returns [null]
-yields!(last_empty, "[last({}[])]", json!([]));
-yields!(last_some, "last(1, 2, 3)", 3);
 
 yields!(logb_inf, "infinite | logb | . == infinite", true);
 yields!(logb_nan, "nan | logb | isnan", true);
@@ -148,38 +120,11 @@ fn nth() {
     give(json!(10), &format!("nth(.; {})", fib), json!(55));
 }
 
-yields!(range_many, "[range(-1, 1; 0, 2)]", json!([-1, -1, 0, 1, 1]));
-
 #[test]
 fn range_reverse() {
     give(json!(null), "[range(1, 2)]", json!([0, 0, 1]));
 
     give(json!(3), "[range(.)] | reverse", json!([2, 1, 0]));
-}
-
-yields!(
-    recurse_update,
-    "[0, [1, 2], 3] | recurse |= (.+1)? // .",
-    json!([1, [2, 3], 4])
-);
-
-// the following tests show that sums are evaluated lazily
-// (otherwise this would not terminate)
-yields!(limit_inf_suml, "[limit(3; recurse(.+1) + 0)]", [0, 1, 2]);
-yields!(limit_inf_sumr, "[limit(3; 0 + recurse(.+1))]", [0, 1, 2]);
-
-yields!(limit_inf_path, "[limit(2; [1] | .[repeat(0)])]", [1, 1]);
-
-#[test]
-fn recurse_obj() {
-    let x = json!({"a":0,"b":[1]});
-    gives(x.clone(), "recurse", [x, json!(0), json!([1]), json!(1)]);
-
-    let y = [json!(1), json!(2), json!(3)];
-    gives(json!(1), "recurse(.+1; . < 4)", y);
-
-    let y = [json!(2), json!(4), json!(16)];
-    gives(json!(2), "recurse(. * .; . < 20)", y);
 }
 
 #[test]
@@ -200,39 +145,10 @@ fn recurse_arr() {
 }
 
 yields!(
-    recurse3,
-    "[1 | recurse(if . < 3 then .+1 else empty end)]",
-    [1, 2, 3]
-);
-yields!(
-    reduce_recurse,
-    "reduce recurse(if . == 1000 then empty else .+1 end) as $x (0; . + $x)",
-    500500
-);
-
-const RECURSE_FLATTEN: &str = "def flatten($d):
-  [ { d: $d, x: . } |
-    recurse(if .d >= 0 and ([] <= .x and .x < {}) then { d: .d - 1, x: .x[] } else empty end) |
-    if .d < 0 or (.x < [] or {} <= .x) then .x else empty end
-  ];";
-
-yields!(
-    recurse_flatten,
-    &(RECURSE_FLATTEN.to_owned() + "[[[1], 2], 3] | flatten(1)"),
-    json!([[1], 2, 3])
-);
-
-yields!(
     recurse_fib_100,
     "def fib: recurse([.[1], add])[0]; nth(100; [0, 1] | fib) | tostring",
     "354224848179261915075"
 );
-
-#[test]
-fn repeat() {
-    let y = json!([0, 1, 0, 1]);
-    give(json!([0, 1]), "[limit(4; repeat(.[]))]", y);
-}
 
 // the implementation of scalb in jq (or the libm.a library) doesn't
 // allow for float exponents; jaq tolerates float exponents in scalb
@@ -297,10 +213,6 @@ yields!(
     "[-123.456, -2.2, -2, -1, 0, 0.00001, 1, 2, 2.2, 123.456] | map(significand)",
     [-1.929, -1.1, -1.0, -1.0, 0.0, 1.31072, 1.0, 1.0, 1.1, 1.929]
 );
-
-yields!(tostring_zero, "1.0 - 1.0 | tostring", "0.0");
-yields!(tostring_str, r#""\n" | tostring"#, "\n");
-yields!(tostring_arr_str, r#"["\n"] | tostring"#, "[\"\\n\"]");
 
 #[test]
 fn typ() {
